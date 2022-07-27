700247db72893cabfe6a59d3a4a231209316473e
Gomathy <105177560+sindugomathi@users.noreply.github.com>
2022-07-27 15:36:21 -0500

    Add files via upload

---
+++ sample4.java
@@ -0,0 +1,62 @@
+package firstlaunch;
+
+public class sample4 {
+	public static void main(String[] args) {
+		int n=7;
+		for(int i=0;i<n;i++)
+		{
+		if(i==0||i>1&&i<=n-1)
+			System.out.print("*");
+			else
+			System.out.print(" ");
+		System.out.print(" ");
+		
+for(int j=0;j<n;j++) {
+	if(i==0&&i==n-1||j==0||j==n-1||i==j)
+		System.out.print("*");
+	else
+	System.out.print(" ");	
+}
+System.out.print(" ");
+for(int j=0;j<n;j++)
+{
+	if(i==0||j==0||i==n/2||i==n-1)
+		System.out.print("*");
+	else
+	System.out.print(" ");	
+}
+System.out.print(" ");
+for(int j=0;j<n;j++)
+{
+	if(j==0&&i!=n-1||j==n-1&&i!=n-1||i==n-1&&j!=0&&j!=n-1)
+		System.out.print("*");
+	else
+	System.out.print(" ");	
+}
+System.out.print(" ");
+for(int j=0;j<n;j++)
+{
+	if(j==0||i==n/2&&j<=n-3||i==0&&j<=n-3||i==n/n&&j==n-2||i==((n/2)-1)&&j==n-2||i==n-1&&j==n-2||i==n-2&&j==((n/2)+1)||i==((n/2)+1)&&j==n/2)
+		System.out.print("*");
+	else
+	System.out.print(" ");	
+}
+System.out.print(" ");
+for(int j=0;j<n;j++)
+{
+	if(i==0&&j!=0&&j!=n-1||j==0&&i!=0&&i!=n-1||j==n-1&&i!=0&&i!=n-1||i==n-1&&j!=0&&j!=n-1)
+		System.out.print("*");
+	else
+	System.out.print(" ");	
+}
+System.out.print(" ");
+for(int j=0;j<n;j++) {
+	if(i==0&&i==n-1||j==0||j==n-1||i==j)
+		System.out.print("*");
+	else
+	System.out.print(" ");	
+}
+System.out.println();
+}
+}
+}
